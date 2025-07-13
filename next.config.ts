65045d382fe8dd211174dc35aa9d09f2015a1fd2
PitchArN <pitchaya.pan01@gmail.com>
2025-07-13 14:27:15 +0700

    Revert "change"
    This reverts commit dfd866bd4e753adf04139d494bc303f73c5bbc7e.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/PitchArN.github.io",
-  images: {
-    unoptimized: true, // necessary for `next export` to work with images
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
