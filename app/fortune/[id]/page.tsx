a569db7ac68ef19cc010b07ed01a841e140168a1
PitchArN <pitchaya.pan01@gmail.com>
2025-07-16 01:48:04 +0700

    Update page.tsx

--- app/fortune/[id]/page.tsx
+++ app/fortune/[id]/page.tsx
@@ -1,6 +1,17 @@
 import FortuneResultPage from '@/components/pages/FortuneResultPage'
 import { use } from 'react'
 
+export async function generateStaticParams() {
+    return [
+        { id: '0' },
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+    ]
+}
+
 export default function ResultPage({
     params,
 }: {
